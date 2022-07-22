7151619be8969fbaa78771f83830c37b6b4e4e8a
Sergey Zagriychuk <>
2022-07-22 16:54:30 +0300

    some changes

--- src/com/solvd/laba/task1/Main2.java
+++ src/com/solvd/laba/task1/Main2.java
@@ -1,11 +1,10 @@
-package com.company;
+package com.solvd.laba.task1;
 
-public class Main {
+public class Main2 {
 
     private int number;
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
     }
 }
